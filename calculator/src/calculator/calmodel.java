c049f4ede81414649c1bd6e468767313597f032d
unknown <f219409@cfd.nu.edu.pk>
2023-10-06 10:38:18 +0500

    new Cal Model

---
+++ calculator/src/calculator/calmodel.java
@@ -0,0 +1,26 @@
+package calculator;
+
+public class calmodel {
+	public double add(double a, double b) {
+		return a + b;
+	}
+
+	public double subtract(double a, double b) {
+		return a - b;
+	}
+
+	public double Multiply(double a, double b) {
+		return a * b;
+	}
+
+	public double Divide(double a, double b) {
+		return a / b;
+	}
+
+	public double Power(double a, double b) {
+		return Math.pow(a, b);
+	}
+	public CalculatorModel() {
+		// TODO Auto-generated constructor stub
+	}
+}
